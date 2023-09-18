a4e6b1005a86444c70ef433ccce89035dbefd248
Sean Hanlon <sean.a.hanlon@gmail.com>
2023-09-18 21:22:31 +0100

    Calculator file complete

--- src/main/java/com/codebase/Calculator.java
+++ src/main/java/com/codebase/Calculator.java
@@ -1,5 +1,20 @@
 package com.codebase;
 
 public class Calculator {
+  public Calculator (){
+  }
+  public static int add(int num1, int num2){
+    return num1 + num2;
+  }
 
+  public static int subtract(int num1, int num2){
+    return num1 - num2;
+  }
+
+  public static int multiply(int num1, int num2){
+    return num1 * num2;
+  }
+  public static int divide(int num1, int num2){
+    return num1 / num2;
+  }
 }
